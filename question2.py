e10675b5a8012262fc8a1a2b97576e86ba7902b0
baohuy2209 <104624325+baohuy2209@users.noreply.github.com>
2024-11-14 08:26:31 +0700

    tests

---
+++ question2.py
@@ -0,0 +1,8 @@
+n = 9 
+for i in range(n): 
+    for j in range(j): 
+        if j == n/2 or i == n/2: 
+            print("+ ", end="")
+        else: 
+            print("  ", end="")
+    print()
